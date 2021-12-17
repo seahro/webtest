98f2f2f3ac0f31d1260f09dbf4807497c4461795
Tim Rawson <rawson.t@gmail.com>
2021-12-17 07:18:40 -0800

    52nd attempt

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -24,7 +24,10 @@ export const cryptoApi = createApi({
         getCryptoHistory: builder.query({
             query: ({ coinId, timePeriod }) => createRequest(`/coin/${coinId}/history/${timePeriod}`),
         }),
+        getExchanges: builder.query({
+            query: () => createRequest(`/exchanges`),
+        }),
     }),
 });
 
-export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery } = cryptoApi;
